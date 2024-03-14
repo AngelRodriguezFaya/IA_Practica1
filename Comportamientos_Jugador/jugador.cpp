e22aa235edc0937d2adbe5a2153b533fb9a59d2e
Angel Rodriguez <arodriguezfaya16@gmail.com>
2024-03-14 17:21:44 +0100

    Nuevas funciones

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -6,8 +6,54 @@ Action ComportamientoJugador::think(Sensores sensores)
 {
 
 	Action accion = actRUN;
+	int a;
 
-	// Mostrar el valor de los sensores
+	// Actualización de las variables de esatdo
+	switch (last_action)
+	{
+	case actWALK:		// Actualización en caso de avanzar
+		switch (current_state.brujula)
+		{
+		case norte: current_state.fil--; break;			
+		case noreste: current_state.fil--; current_state.col++; break;
+		case este: current_state.col++; break;
+		case sureste: current_state.fil++; current_state.col++; break;
+		case sur: current_state.fil++; break;
+		case suroeste: current_state.fil++; current_state.col--; break;
+		case oeste: current_state.col--; break;
+		case noroeste: current_state.fil--; current_state.col--; break;
+		}
+		break;
+	
+	case actRUN:		// Actualización en caso de correr
+		break;
+	
+	case actTURN_SR:	// Actualización en caso de girar 45º a la derecha
+		a = current_state.brujula;
+		a = (a + 1) % 8;
+		current_state.brujula = static_cast<Orientacion>(a);
+		break;
+	
+	case actTURN_L:	   // Actualización en caso de girar 90º a la izquierda
+		a = current_state.brujula;
+		a = (a - 6) % 8;	// Es equivalente a (a-2+8) % 8.
+		current_state.brujula = static_cast<Orientacion>(a);
+		break;
+	}
+
+	// Decidir la nueva accion
+	if( (sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S') // Si puedo avanzar a la siguiente casilla
+		and sensores.agentes[2] == '_' ){
+			accion = actWALK;
+	} else if(!girar_derecha){	
+		accion = actTURN_L;
+		girar_derecha = (rand()%2 == 0);
+	} else {
+		accion = actTURN_SR;
+		girar_derecha = (rand()%2 == 0);
+	}
+
+/* 	// Mostrar el valor de los sensores
 	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC;
 	switch (sensores.sentido)
 	{
@@ -30,9 +76,13 @@ Action ComportamientoJugador::think(Sensores sensores)
 
 	cout << "\nColision: " << sensores.colision;
 	cout << "  Reset: " << sensores.reset;
-	cout << "  Vida: " << sensores.vida << endl<< endl;
+	cout << "  Vida: " << sensores.vida << endl<< endl; */
+
+	// Recordar la ultima accion
+	last_action = accion;
 
 	return accion;
+	
 }
 
 int ComportamientoJugador::interact(Action accion, int valor)
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -4,14 +4,26 @@
 #include "comportamientos/comportamiento.hpp"
 using namespace std;
 
+struct state // Estado
+{
+  int fil;
+  int col;
+  Orientacion brujula;
+};
+
+
 class ComportamientoJugador : public Comportamiento{
 
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
-      // Dar el valor inicial a las variables de estado
-    }
-
+      current_state.fil = 99;
+      current_state.col = 99;
+      current_state.brujula = norte;
+      last_action = actIDLE;
+      girar_derecha = false;
+     }
+     
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
     ~ComportamientoJugador(){}
 
@@ -20,5 +32,8 @@ class ComportamientoJugador : public Comportamiento{
 
   private:
   // Declarar aquí las variables de estado
+  state current_state; Orientacion brujula;
+  Action last_action;
+  bool girar_derecha;
 };
 #endif
